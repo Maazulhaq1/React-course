860ff4d2b28a1a230db54c3b7614693438859b78
Maaz ul haq <104296973+Maazulhaq1@users.noreply.github.com>
2022-04-28 05:35:37 +0500

    Add files via upload

---
+++ About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nisi iure fugiat id odio saepe fugit ab consectetur laudantium sit explicabo necessitatibus delectus facere quaerat quas ducimus ratione voluptatibus aliquid, possimus, maxime magnam sapiente?</p>
+    </div>
+  )
+}
+
+export default About
